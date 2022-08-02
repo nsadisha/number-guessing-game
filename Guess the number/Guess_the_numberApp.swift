86a7331e26bd961a976f254c511308d90050bb43
Sadisha Nimsara <nsadisha@gmail.com>
2022-08-02 11:52:40 +0530

    Initial Commit

---
+++ Guess the number/Guess_the_numberApp.swift
@@ -0,0 +1,17 @@
+//
+//  Guess_the_numberApp.swift
+//  Guess the number
+//
+//  Created by Sadisha Nimsara on 2022-08-02.
+//
+
+import SwiftUI
+
+@main
+struct Guess_the_numberApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
